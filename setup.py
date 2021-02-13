814ec53572ff5c013f0de66f22b0edf0a0588f53
Francisco Nogueira <43782150+francisco-alier@users.noreply.github.com>
2021-02-13 18:17:38 +0000

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='logistic_implementation',
-      version='0.1',
-      description='Implementation of Logist Regression algorithm using OOP',
-      packages=['logistic_implementation'],
-      zip_safe=False)
